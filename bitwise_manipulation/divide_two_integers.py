cf5172082f0112481ca26a8298c080d298bdf470
Zhaoyu Zhang <zhangzhaoyu@yahoo.com>
2021-10-10 18:51:43 +0800

    add new bit manipulation problem

---
+++ bitwise_manipulation/divide_two_integers.py
@@ -0,0 +1,24 @@
+"""
+Bitwise Manipulation - Divide Two Integers (medium)
+
+Description:
+Given two integers dividend and divisor, divide two integers without using multiplication, 
+division, and mod operator.
+
+Return the quotient after dividing dividend by divisor.
+
+The integer division should truncate toward zero, which means losing its fractional part. 
+For example, truncate(8.345) = 8 and truncate(-2.7335) = -2.
+
+Note: Assume we are dealing with an environment that could only store integers within the 
+32-bit signed integer range: [−2^31, 2^31 − 1]. For this problem, assume that your function 
+returns 2^31 − 1 when the division result overflows.
+
+Example:
+
+Input: dividend = 10, divisor = 3
+Output: 3
+Explanation: 10/3 = truncate(3.33333..) = 3.
+
+LeetCode: https://leetcode-cn.com/problems/divide-two-integers
+"""
